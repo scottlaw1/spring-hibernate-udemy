e0a81892115fdd69605a12967a4f349e3508377d
Scott A. Lawrence <slawrence@gmail.com>
2017-05-25 23:24:03 -0400

    Section 5.33
    Just completed the constructor injection examples.

---
+++ spring-demo-one/src/com/luv2code/springdemo/BasketballCoach.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+public class BasketballCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		return "Make 20 self-pass turn around jump shots in 2 minutes.";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
